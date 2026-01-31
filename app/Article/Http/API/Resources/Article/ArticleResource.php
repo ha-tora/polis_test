40cb0fc1a7b5764374631db1382904ab2dc16953
ha_tora <j.2374.04@gmail.com>
2026-01-31 23:16:58 +0600

    article resource class name fixed

--- app/Article/Http/API/Resources/Article/ArticleResource.php
+++ app/Article/Http/API/Resources/Article/ArticleResource.php
@@ -5,7 +5,7 @@ namespace App\Article\Http\API\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ArticleResourse extends JsonResource
+class ArticleResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
