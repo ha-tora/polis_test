201c1b8d06694670abb52d7f6c4667ce688df541
ha_tora <j.2374.04@gmail.com>
2026-01-31 23:15:15 +0600

    comment request added

---
+++ app/Article/Http/API/Requests/Comment/StoreCommentRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Article\Http\API\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'article_id'    => ['required', 'exists:article,id'],
+            'author_name'   => ['required', 'string'],
+            'content'       => ['required', 'string']
+        ];
+    }
+}
