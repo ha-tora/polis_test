f6db9fc4e3ba24e4801ad4105b60075a66a968ca
ha_tora <j.2374.04@gmail.com>
2026-01-31 23:42:08 +0600

    api routes added

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Article\Http\API\Controllers\CommentController;
+use App\Article\Http\API\Resources\ArticleCollection;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('articles')->name('articles.')->group(function () {
+   Route::get('/', [ArticleCollection::class, 'index'])->name('index');
+   Route::get('/{article}', [ArticleCollection::class, 'show'])->name('show');
+   Route::post('/', [ArticleCollection::class, 'store'])->name('store');
+
+   Route::post('/{article}/comments', [CommentController::class, 'store'])->name('comments.store');
+});
