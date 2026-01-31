4629958ec875be41c1efba2628e3222f2bfdf773
ha_tora <j.2374.04@gmail.com>
2026-02-01 00:24:17 +0600

    namespaces fix

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Article\Http\API\Controllers\ArticleController;
 use App\Article\Http\API\Controllers\CommentController;
-use App\Article\Http\API\Resources\ArticleCollection;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('articles')->name('articles.')->group(function () {
-   Route::get('/', [ArticleCollection::class, 'index'])->name('index');
-   Route::get('/{article}', [ArticleCollection::class, 'show'])->name('show');
-   Route::post('/', [ArticleCollection::class, 'store'])->name('store');
+   Route::get('/', [ArticleController::class, 'index'])->name('index');
+   Route::get('/{article}', [ArticleController::class, 'show'])->name('show');
+   Route::post('/', [ArticleController::class, 'store'])->name('store');
 
    Route::post('/{article}/comments', [CommentController::class, 'store'])->name('comments.store');
 });
